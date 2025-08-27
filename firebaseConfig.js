41a30112b0b88e5d5611fe78650fca4a7c312e54
Wardo30 <jowardremeticado30@gmail.com>
2025-08-27 09:47:48 +0800

    index changed

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjji487HtaM_0AVSvspezMC7Y_fbzveyg",
+  authDomain: "jwrd-goals.firebaseapp.com",
+  projectId: "jwrd-goals",
+  storageBucket: "jwrd-goals.firebasestorage.app",
+  messagingSenderId: "5273182668",
+  appId: "1:5273182668:web:0d581c56567c104f03bd69"
+};
+
+const app = initializeApp(firebaseConfig);
