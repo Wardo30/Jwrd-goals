a7ec8d8c60c60185e3e4c5907bf155000e6fc7e9
carlocanezares <xxdjcarloxx@gmail.com>
2025-09-03 09:09:20 +0800

    console log

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCjji487HtaM_0AVSvspezMC7Y_fbzveyg",
   authDomain: "jwrd-goals.firebaseapp.com",
@@ -10,3 +10,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
